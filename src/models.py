6019c07c1f274caee2e8b1ae608a2a482350ab38
Enes004 <enessatici20@gmail.com>
2025-12-09 19:57:47 +0300

    Station class created

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 class Station:
-    def __init__(self,id,name,x,y):
+    def __init__(self,id,name,x_loc,y_loc):
         self.id = id
         self.name = name
-        self.location = (x,y)
+        self.location = (x_loc,y_loc)
         self.neighbors = {}
+
+    def add_neighbor(self,neighbor_id,duration):
+        self.neighbors[neighbor_id]=duration
