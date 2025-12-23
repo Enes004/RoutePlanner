0ea5ed8b6e1c955bd68904a248522451b0e1442d
Enes004 <enessatici20@gmail.com>
2025-12-23 18:50:29 +0300

    binary search algorithm added

---
+++ src/algorithms/binary_searching.py
@@ -0,0 +1,28 @@
+
+class StationSearcher:
+    def __init__(self):
+        pass
+
+    def Binary_Search(self,sorted_list,target_name):
+
+        target_name = target_name.lower()
+        left = 0
+        right = len(sorted_list)-1
+
+        while (left <= right):
+            mid = len(sorted_list)//2
+            current_station = sorted_list[mid]
+            current_name = current_station.name.lower()
+
+            if current_name == target_name:
+                return current_station
+            
+            elif current_name < target_name:
+                left = mid+1
+
+            else:
+                right = mid-1
+                
+        return None
+
+
