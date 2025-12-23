7f1893581f0f4a665ef4e4d0a703832434249cdc
Enes004 <enessatici20@gmail.com>
2025-12-23 17:35:43 +0300

    quick sort algorithm added

---
+++ src/algorithms/quick_sorting.py
@@ -0,0 +1,18 @@
+
+
+class StationSorter(self):
+    def quick_sort_stations(self,stations_in_json):
+
+        if len(stations_in_json)<=1:
+            return stations_in_json
+        
+        pivot_index = len(stations_in_json)//2              
+        pivot_station = stations_in_json[pivot_index]   ## Choose the middle element of the list
+        pivot_name = pivot_station.name.lower() ## Sort alphabetically and convert to lowercase for ASCII-based comparison
+
+        # list compherations
+        left = [station_object for station_object in stations_in_json if station_object.name.lower() < pivot_name]
+        middle = [station_object for station_object in stations_in_json if station_object.name.lower() == pivot_name]
+        right =  [station_object for station_object in stations_in_json if station_object.name.lower() > pivot_name]
+
+        return self.quick_sort_stations(left) + middle + self.quick_sort_stations(right)
