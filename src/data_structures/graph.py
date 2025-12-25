844a12d490097cf4b9ce0cf032f591dbae7d5aa6
Enes004 <enessatici20@gmail.com>
2025-12-25 16:55:35 +0300

    graph commits added

--- src/data_structures/graph.py
+++ src/data_structures/graph.py
@@ -1,22 +1,23 @@
 class Graph:
     def __init__(self):
+        #{ "Durak İsmi": [ (Komşu, Mesafe), (Diğer Komşu, Mesafe) ] }
         self.adj_list = {}
 
     def add_node(self, node):
-        """Durağı sisteme ekler."""
         if node not in self.adj_list:
-            self.adj_list[node] = []
+            self.adj_list[node] = [] # "Üsküdar" satırını aç, komşuları henüz boş kalsın.
 
     def add_edge(self, u, v, weight):
-        """İki durak arasında bağlantı kurar."""
+        #Önce durakları ekle
         self.add_node(u)
         self.add_node(v)
-        # Liste içinde tuple tutuyoruz: (komşu, süre)
+
+        #Birbirlerine komşuluklarını ve ağırlıklarını ekle
         if (v, weight) not in self.adj_list[u]:
             self.adj_list[u].append((v, weight))
+
         if (u, weight) not in self.adj_list[v]:
             self.adj_list[v].append((u, weight))
 
     def get_neighbors(self, node):
-        """Komşuları liste olarak döndürür."""
         return self.adj_list.get(node, [])
